50042f56d6340bb860f496053cdf78fa9fa1ffea
Theodore Hammar <theodore.hammar@elev.ga.ntig.se>
2024-03-21 13:57:21 +0100

    A uppgift

---
+++ script.js
@@ -0,0 +1,20 @@
+// function start_animation() {
+//   document.body.innerhtml="";
+//   let array = document.body.children;
+//   if (!array) {
+//     console.log("inga element");
+//     return;
+//   }
+
+//   for (let i = 0; i < array.length; i++) {
+//     array[i].remove();
+//   }
+
+//   let h1 = document.createElement("h1");
+//   h1.textContent = "Lurad!!!!!";
+//   let p = document.createElement("h1");
+//   p.textContent = "Detta är ett 'dåligt' bibliotek";
+//   document.body.appendChild(h1);
+//   document.body.appendChild(p);
+//   document.body.style.backgroundColor = "red";
+// }
